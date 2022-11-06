c92ee02075383b9f5d01b38e469b3421ff78646b
Sjors Boom <s.boom@betabit.nl>
2022-11-06 17:07:44 +0100

    Try and fix the Counter test

---
+++ PlaywrightTest1/BlazorDemo/Counter/CounterPageTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using PlaywrightTest1.TestBaseClasses;
+using System.Threading.Tasks;
+
+namespace PlaywrightTest1.BlazorDemo.Counter
+{
+    [Parallelizable(ParallelScope.Self)]
+    public class CounterPageTest : PageTestBase
+    {
+        [Test]
+        public async Task CheckIncrement()
+        {
+            CounterPageObject counterPage = _playwrightServiceProvider.IPageObjectGetPageObject<CounterPageObject>(Page);
+            await counterPage.OpenAsync();
+
+            await Expect(counterPage.CurrentCount).ToHaveTextAsync("0");
+            for (int i = 0; i < 10; i++)
+            {
+                await counterPage.IncementButton.ClickAsync();
+                await Expect(counterPage.CurrentCount).ToHaveTextAsync($"{i + 1}");
+            }
+        }
+    }
+}
