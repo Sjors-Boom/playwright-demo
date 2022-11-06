d90712d552d69a371686b46a8ac619eb491d5ff7
Sjors Boom <s.boom@betabit.nl>
2022-11-06 17:12:44 +0100

    Finish the DelayPageObject class

---
+++ PlaywrightTest1/BlazorDemo/Delay/DelayTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using PlaywrightTest1.TestBaseClasses;
+using System.Threading.Tasks;
+
+namespace PlaywrightTest1.BlazorDemo.Delay
+{
+    [Parallelizable(ParallelScope.Self)]
+    public class DelayTests : PageTestBase
+    {
+        [Test]
+        public async Task PushTheButton()
+        {
+            DelayPageObject sut = _playwrightServiceProvider.IPageObjectGetPageObject<DelayPageObject>(Page);
+            await sut.OpenAsync();
+
+            await sut.Button.ClickAsync();
+
+            await Expect(sut.SuccesMessage).ToBeVisibleAsync();
+        }
+    }
+}
