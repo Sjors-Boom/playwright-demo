7bffdb3b1d95cd09591bcc0815610802050967ad
Sjors Boom <s.boom@betabit.nl>
2022-11-06 17:07:44 +0100

    Try and fix the Counter test

---
+++ PlaywrightTest1/BlazorDemo/Counter/CounterPageObject.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+using PlaywrightTest1.Helpers;
+using PlaywrightTest1.PageObjects;
+using System.Threading.Tasks;
+
+namespace PlaywrightTest1.BlazorDemo.Counter
+{
+    public class CounterPageObject : BasePageObject
+    {
+        public ILocator IncementButton => _page.Locator("#incrementCountbutton");
+        public ILocator CurrentCount => _page.Locator("currentCount");
+        public CounterPageObject(UrlService urlService) : base(urlService)
+        { }
+
+        public override async Task OpenAsync()
+        {
+            await Navigate("counter");
+        }
+    }
+}
