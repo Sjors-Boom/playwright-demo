1467e3a5ff9cc73903fb317a385cabd6c673a25f
Sjors Boom <s.boom@betabit.nl>
2022-11-06 17:21:20 +0100

    Build the Selector for the RandomElements

---
+++ PlaywrightTest1/BlazorDemo/RandomElement/RandomElementPageObject.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Playwright;
+using PlaywrightTest1.Helpers;
+using PlaywrightTest1.PageObjects;
+using System.Threading.Tasks;
+
+namespace PlaywrightTest1.BlazorDemo.RandomElement
+{
+    public class RandomElementPageObject : BasePageObject
+    {
+        public ILocator RandomElementSelectedState => _page.Locator("#randomElementSelectedState");
+        // Sometimes you dont have control over the target
+        // so you need to find a different means to differentiate the target
+        // Hint: https://www.w3schools.com/css/css_attribute_selectors.asp
+        public ILocator RelevantDropdowns => default!;
+        public RandomElementPageObject(UrlService urlService)
+            : base(urlService)
+        { }
+
+        public int? CountParam { get; set; }
+
+        public override async Task OpenAsync()
+        {
+            if (CountParam == null)
+            {
+                await Navigate("randomElement");
+            }
+            else
+            {
+                await Navigate($"randomElement/{CountParam}");
+            }
+        }
+    }
+}
