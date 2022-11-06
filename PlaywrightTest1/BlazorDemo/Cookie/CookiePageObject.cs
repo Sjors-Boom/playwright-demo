c3e87e3e64e0c77e610d9e56c02306e0a9a5d0f6
Sjors Boom <s.boom@betabit.nl>
2022-11-06 17:36:22 +0100

    Cookie test added

---
+++ PlaywrightTest1/BlazorDemo/Cookie/CookiePageObject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Playwright;
+using PlaywrightTest1.Helpers;
+using PlaywrightTest1.PageObjects;
+using System.Threading.Tasks;
+
+namespace PlaywrightTest1.BlazorDemo.Cookie
+{
+    public class CookiePageObject : BasePageObject
+    {
+        public ILocator CookieValue => _page.Locator("#cookieValue");
+        public CookiePageObject(UrlService urlService) : base(urlService)
+        { }
+
+        public override async Task OpenAsync()
+        {
+            await Navigate("cookie");
+        }
+    }
+}
