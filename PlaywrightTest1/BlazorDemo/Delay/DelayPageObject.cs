99de7cb65646d30e0d3a811ccb872b68b44dad36
Sjors Boom <s.boom@betabit.nl>
2022-11-06 17:12:44 +0100

    Finish the DelayPageObject class

---
+++ PlaywrightTest1/BlazorDemo/Delay/DelayPageObject.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+using PlaywrightTest1.Helpers;
+using PlaywrightTest1.PageObjects;
+using System.Threading.Tasks;
+
+namespace PlaywrightTest1.BlazorDemo.Delay
+{
+    public class DelayPageObject : BasePageObject
+    {
+        public ILocator Button => default!;
+        public ILocator SuccesMessage => default!;// Hint: Build SuccesMessageLocator with the SnackbarLocatorService
+        public ISnackbarLocatorService SnackbarLocatorService { get; set; }
+        public DelayPageObject(UrlService urlService, ISnackbarLocatorService snackbarLocatorService)
+            : base(urlService)
+        {
+            SnackbarLocatorService = snackbarLocatorService;
+        }
+
+        public override Task OpenAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
