8252b7871c050424040c83c979e233a735c17440
Sjors Boom <s.boom@betabit.nl>
2022-11-06 17:32:51 +0100

    Finish Wizardtest

---
+++ PlaywrightTest1/BlazorDemo/Wizard/WizardPageObject.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Playwright;
+using Newtonsoft.Json;
+using PlaywrightTest1.Helpers;
+using PlaywrightTest1.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PlaywrightTest1.BlazorDemo.Wizard
+{
+    public class WizardPageObject : BasePageObject
+    {
+        public ILocator Table => _page.Locator("#wizardTable");
+        public ILocator NextButton => _page.Locator("#next");
+        public ILocator PreviousButton => _page.Locator("#previous");
+        public ILocator WizardStartMessage => _page.Locator("#previous");
+        public ILocator WizardEndMessage => _page.Locator("#previous");
+        public ILocator WizardField => _page.Locator("input[type=text]");
+
+        public IDictionary<string, string>? TemplateParam { get; internal set; }
+
+        public WizardPageObject(UrlService urlService, IServiceProvider serviceProvider)
+            : base(urlService)
+        { }
+
+        public override async Task OpenAsync()
+        {
+            if (TemplateParam == null)
+            {
+                await Navigate("wizard");
+            }
+            else
+            {
+                await Navigate($"wizard/{JsonConvert.SerializeObject(TemplateParam)}");
+            }
+        }
+
+        public ILocator TableValue(string key)
+        {
+            return Table.Locator("tbody tr")
+                .Filter(new()
+                {
+                    Has = _page.Locator("td")
+                        .Filter(new()
+                        {
+                            HasTextString = key
+                        })
+                })
+                .Locator("td:nth-of-type(2)");
+        }
+    }
+}
---
+++ PlaywrightTest1/BlazorDemo/Wizard/WizardTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using PlaywrightTest1.TestBaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlaywrightTest1.BlazorDemo.Wizard
+{
+    [Parallelizable(ParallelScope.Self)]
+    public class WizardTest : PageTestBase
+    {
+        [Test]
+        [TestCaseSource(nameof(WizardTestCases))]
+        public async Task FillWizard(TestCase testcase)
+        {
+            WizardPageObject sut = _playwrightServiceProvider.IPageObjectGetPageObject<WizardPageObject>(Page);
+            sut.TemplateParam = testcase.Template;
+            await sut.OpenAsync();
+
+            testcase.Template ??= new Dictionary<string, string>()
+            {
+                { "Field 1", "" },
+                { "Field 2", "default value" },
+                { "Field 3", "" }
+            };
+
+            await Expect(sut.PreviousButton).ToBeDisabledAsync();
+            await Expect(sut.WizardStartMessage).ToBeVisibleAsync();
+            await sut.NextButton.ClickAsync();
+
+            foreach (KeyValuePair<string, string> pair in testcase.Template)
+            {
+                //Random new value to fill in field
+                string value = Guid.NewGuid().ToString();
+
+                //Locator to find the value of the model in the table
+                ILocator tableValueLocator = sut.TableValue(pair.Key);
+
+                //Check current value
+                //TODO in table tableValueLocator
+                //TODO in input field
+
+                //Update to new value
+                //TODO fill field with new value
+
+                await sut.NextButton.ClickAsync();
+
+                //Check if new value has updated in table
+                //TODO 
+            }
+            await Expect(sut.WizardEndMessage).ToBeVisibleAsync();
+            await Expect(sut.NextButton).ToBeDisabledAsync();
+
+        }
+
+        //This method generates 3 different testcases for the above test
+        public static IEnumerable<TestCase> WizardTestCases()
+        {
+            yield return new() { Name = "TestDataNull", Template = null };
+            yield return new() { Name = "GenericDefaultValue", Template = new Dictionary<string, string>() { { "Empty", "" }, { "WithDefaultValue", "DefaultValue" }, { "Integer", "1" } } };
+            yield return new() { Name = "LargeSet", Template = Enumerable.Range(0, 25).ToDictionary(x => Guid.NewGuid().ToString(), x => x % 2 == 0 ? "" : "odd") };
+        }
+
+        public class TestCase
+        {
+            public string Name { get; set; }
+            public IDictionary<string, string> Template { get; set; }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+    }
+}
